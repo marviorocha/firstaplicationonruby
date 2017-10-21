0b44e88308010fa3b761bfb84337180232c3b250
marviorocha <marviorocha@marviorocha.com>
2017-10-20 23:40:29 -0200

    Add users table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 #My resources for my application writer on rails
 resources "articles"
+resources "users"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root'pages#home'
 get 'about', to: 'pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013184634) do
+ActiveRecord::Schema.define(version: 20171021013842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
