c337f70ec00c23ef4787a11bcbd54be28855c7c5
marviorocha <marviorocha@marviorocha.com>
2017-10-22 02:20:03 -0200

    Add advise login logout etc...

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '~> 5.0'
 gem 'jquery-rails'
-
+gem 'devise', '~> 4.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+
+devise_for :users
 #My resources for my application writer on rails
 resources "articles"
-resources "users"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root'pages#home'
 get 'about', to: 'pages#about'
