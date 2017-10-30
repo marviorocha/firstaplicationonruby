a99594d95ef14d6e3aaa7f3484b7046bb58b7790
marviorocha <marviorocha@marviorocha.com>
2017-10-30 18:52:29 -0200

    Add advise and flash notification

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
 devise_for :users
 #My resources for my application writer on rails
-resources "articles"
+resources :articles
+
+
+root'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root'pages#home'
+
+get 'blog', to: 'pages#blog'
 get 'about', to: 'pages#about'
 end
